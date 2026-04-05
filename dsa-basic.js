e843b4840c2d34ef8f22b0f65f858a34520bc5f6
soroardiislam <mdsoroardi602828@gmail.com>
2026-04-05 22:06:41 +0600

    unary

---
+++ dsa-basic.js
@@ -0,0 +1,19 @@
+// unary operator ++ --
+let a = 10;
+
+let b = a++;
+let c = b;
+console.log(b);
+console.log(c);
+
+//Ans: 10
+//Ans: 10
+//++++++----------------------+++++++++++++
+
+let x = 10;
+
+let y = ++x;
+
+console.log(y);
+
+//Ans: 11
