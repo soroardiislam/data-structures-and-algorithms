810490b1311cf75c03c73361ef60d59a90ab3f4f
soroardiislam <mdsoroardi602828@gmail.com>
2026-04-08 21:18:12 +0600

    update

--- dsa-basic.js
+++ dsa-basic.js
@@ -1,19 +1,62 @@
-// unary operator ++ --
-let a = 10;
+// // unary operator ++ --
+// let a = 10;
 
-let b = a++;
-let c = b;
-console.log(b);
-console.log(c);
+// let b = a++;
+// let c = b;
+// console.log(b);
+// console.log(c);
 
-//Ans: 10
-//Ans: 10
-//++++++----------------------+++++++++++++
+// //Ans: 10
+// //Ans: 10
+// //++++++----------------------+++++++++++++
 
-let x = 10;
+// let x = 10;
 
-let y = ++x;
+// let y = ++x;
 
-console.log(y);
+// console.log(y);
 
-//Ans: 11
+// //Ans: 11
+
+
+
+a = 1++ * ++1;
+
+console.log(a);
+
+
+// let a 11, b = 22;
+
+// let ca+b+a+++ b++++++++b;
+
+// console.log("a=" + a);
+
+// console.log("b=" + b);
+
+// console.log("c=" + c);
+
+// I
+
+// Q.
+
+// JavaScript
+
+// let b = true;
+
+// b++;
+
+// console.log(b);
+
+// Q.
+
+// Q.
+
+// let a = 11++;
+
+// console.log(a);
+
+// let 1 = 11;
+
+// let j=(1++);
+
+// Math functions
